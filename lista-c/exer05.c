fccaf7765ce2d55497c88c83fc69cb10f99aed5e
jefersonecruz-commits <jeferson.e.cruz@aluno.senai.br>
2025-12-01 16:52:38 -0300

    Add files via upload

---
+++ lista-c/exer05.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+int main(){
+setlocale(LC_ALL, "portuguese");
+
+    float peso1,peso2,media;
+
+    
+    printf("Digite o peso da primeira pessoa: ");
+    scanf(" %f", &peso1);
+
+    printf("digite peso da segunda pessoa: ");
+    scanf(" %f", &peso2);
+
+    media = (peso1 + peso2)/2;
+
+    printf("sua media é: %f\n",media);
+
+    system("pause");
+    return 0;
+}
